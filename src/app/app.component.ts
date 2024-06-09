d8dfbc6bd5ec33f17a9e10778251ba42f62dcdc8
Iqbal <iqbalismail5962@gmail.com>
2024-06-09 14:23:46 +0800

    Added header(can delete), navbar(can delete), material-list, modal, quiz-list.  Navbar can delete, it's there just for my reference.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [HeaderComponent, NavbarComponent, RouterModule, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { QuizListComponent } from './quiz-list/quiz-list.component';
+import { MaterialListComponent } from './material-list/material-list.component';
 
 export const routes: Routes = [
     { path: "user", component: UserListComponent },
-    { path: ".", component: UserListComponent }
+    { path: ".", component: UserListComponent },
+    { path: 'quiz', component: QuizListComponent },
+    { path: 'materials', component: MaterialListComponent }
 ];
