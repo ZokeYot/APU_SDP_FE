fd12a697c96fa4fa21df84a5832ad918ef2be3ab
Iqbal <iqbalismail5962@gmail.com>
2024-06-09 16:49:16 +0800

    Added material-list, material-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
 import { MaterialListComponent } from './material-list/material-list.component';
+import { MaterialDetailsComponent } from './material-details/material-details.component';
 
 export const routes: Routes = [
     { path: "user", component: UserListComponent },
     { path: ".", component: UserListComponent },
     { path: 'quiz', component: QuizListComponent },
-    { path: 'materials', component: MaterialListComponent }
+    { path: 'materials', component: MaterialListComponent },
+    { path: 'material-details/:title/:filename/:date', component: MaterialDetailsComponent}
 ];
