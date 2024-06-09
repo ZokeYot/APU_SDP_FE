3f8576c3ea01fccb8613858b11bbfe98903a79f8
JASON-1213 <jasongan041213@gmail.com>
2024-06-09 11:16:13 +0800

    Z

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { RegisterComponent } from './section/register/register.component';
+import { LoginComponent } from './section/login/login.component';
+import { ProfileComponent } from './section/profile/profile.component';
+import { FindComponent } from './section/find/find.component';
+import { PasswordComponent } from './section/password/password.component';
+import { UpdateComponent } from './section/update/update.component';
 
 export const routes: Routes = [
     { path: "user", component: UserListComponent },
-    { path: ".", component: UserListComponent }
+    { path: ".", component: UserListComponent },
+    { path: "register", component: RegisterComponent },
+    { path: "", component: LoginComponent },
+    { path: "login", component: LoginComponent },
+    { path: "", component: RegisterComponent },
+    { path: "find", component: FindComponent },
+    { path: "password", component: PasswordComponent },
+    { path: "profile", component: ProfileComponent },
+    { path: "update", component: UpdateComponent }
 ];
--- src/app/section/register/register.component.ts
+++ src/app/section/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+   
 }
