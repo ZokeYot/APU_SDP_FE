1dde877ba09c689ec7e72903e7dd590a89f94864
ZokeYot <finalboss2004@gmail.com>
2024-06-10 09:36:20 +0800

    Solved Merge Conflict

--- src/app/section/register/register.component.ts
+++ src/app/section/register/register.component.ts
@@ -5,11 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterModule],
-=======
   imports: [CommonModule, RouterModule],
->>>>>>> 28c31999b9113fd277e3db88d7bec29e4571892e
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
