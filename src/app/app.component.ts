7c048b54e44568e8ee4f765781028faef48972ed
ZokeYot <finalboss2004@gmail.com>
2024-06-10 08:48:40 +0800

    Complete the structure of the create quiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './section/header/header.component';
+import { LecturerNavbarComponent } from './section/lectuerer/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { StudentNavbarComponent } from "./section/student/navbar/navbar.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, RouterModule, CommonModule, RouterOutlet, FormsModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, StudentNavbarComponent, RouterModule, CommonModule, RouterOutlet, FormsModule, RouterModule, LecturerNavbarComponent]
 })
 export class AppComponent {
   title = 'SDP_FE';
+  loggedIn = true;
+  isStudent = false;
+  isLecturer = true;
+
+
+
+
 
 
 }
