c0d2b346116da91bdd35997a6257e1049189824d
Dylan <dylanlam3804@gmail.com>
2024-06-18 15:27:08 +0800

    message

--- src/app/section/student/shop/shop.component.ts
+++ src/app/section/student/shop/shop.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+switchPage(arg0: string) {
+throw new Error('Method not implemented.');
+}
 
 }
