5a886ce3401a63301fe8904c14ce662ef7c71f56
Iqbal <iqbalismail5962@gmail.com>
2024-06-19 20:45:08 +0800

    Added home page for student. Smthing happened to package-lock.json (not sure what i modified but it says that)

---
+++ src/app/section/student-main-page/student-main-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-student-main-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './student-main-page.component.html',
+  styleUrl: './student-main-page.component.css'
+})
+export class StudentMainPageComponent {
+
+}
